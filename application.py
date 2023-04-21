79fc58df2634f6f48b5af15addbfd9f998c46f2d
Ken Nguyen <130008166+knguy214@users.noreply.github.com>
2023-04-21 17:21:43 -0400

    Update application.py

--- application.py
+++ application.py
@@ -15,3 +15,27 @@ They would have all their contact information with a click of a few buttons.
 
 Challenges Encountered: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
+import tkinter as tk
+
+class Application(tk.Frame):
+
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()  
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.mondialLabel = tk.Label(self, text='Hello World') #create attribute
+        self.mondialLabel.config(bg = "#00ffff") #give it unique properties
+        self.mondialLabel.grid() #place it on grid
+
+
+        self.quitButton = tk.Button(self, text='Quit', command = add_number)
+        self.quitButton.grid()
+
+def add_number():
+    print(5)
+
+app = Application()
+app.master.title('Sample application')
+app.mainloop()
