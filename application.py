0d7410c16c1165bdcc68d71a510935e76d12ebde
zzhou42 <zhxnnie@gmail.com>
2023-04-21 02:31:44 -0400

    Create application.py

---
+++ application.py
@@ -0,0 +1,17 @@
+"""
+Assignment: Final Project
+Date: 4_21_23
+
+Colin Clifford - Group Leader, Database, Navigator
+Chima Okere - Database, Debugger/Tester
+Ken Nguyen - UI/GUI, Debugger/Tester
+Zhen Zhou - Navigator, UI/GUI
+
+Create an application where users are able to create/edit an account 
+and store contact information through a user interface. This application 
+will allow users to have an organized database of their contacts, 
+allowing users to not have to frantically look for a specific contact. 
+They would have all their contact information with a click of a few buttons.
+
+Challenges Encountered: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
